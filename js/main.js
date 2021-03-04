e8cb5e82e29b451ae2202ae248e6b85c7faf7799
vanplace <70973002+vanplace@users.noreply.github.com>
2021-03-04 19:38:57 +0300

    2++

--- js/main.js
+++ js/main.js
@@ -10,27 +10,77 @@ const check = function ( lineNumber, maxLength) {
 check('track', 4);
 
 //новое задание
-
-
-const id = [
+const IDS = [
   1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,
 ]
-const url = [
+
+function mixId() {
+  const array = IDS.slice(0);
+  for (let i = (array.length - 1); i > 0; i -= 1)
+  {
+    const randomIndex = Math.floor(Math.random() * (i + 1));
+    [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
+  }
+  let mixed = array;
+  for (let i = 0; i <= array.length - 1; i++) {
+    mixed = array[i]
+  }
+  return (
+    mixed)
+}
+mixId()
+
+const URLS = [
   1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,
 ]
-const description = [
+
+function mixUrls() {
+  const array = URLS.slice(0);
+  for (let i = (array.length - 1); i > 0; i -= 1)
+  {
+    const randomIndex = Math.floor(Math.random() * (i + 1));
+    [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
+  }
+  let mixed = array;
+  for (let i = 0; i <= array.length - 1; i++) {
+    mixed = array[i]
+  }
+  return [
+    mixed]
+}
+
+const DESCRIPTION = [
   'Я дома',
   'Я гуляю',
   'Я работаю',
 ]
 
-const idComments= [
+const ID_COMMENTS= [
   1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,
 ]
-const avatar = [
+function mixIdComments() {
+  const array = ID_COMMENTS.slice(0);
+  for (let i = (array.length - 1); i > 0; i -= 1)
+  {
+    const randomIndex = Math.floor(Math.random() * (i + 1));
+    [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
+  }
+  let mixed = array;
+  for (let i = 0; i <= array.length - 1; i++) {
+    mixed = array[i]
+  }
+  return (
+    mixed)
+}
+
+
+mixIdComments()
+
+const AVATAR = [
   1,2,3,4,5,6,
 ]
-const message = [
+
+const MESSAGE = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -45,32 +95,33 @@ const name = [
   'Серафим',
 ]
 
+// создание комментов
+
 const createComments = () => {
-  let randomIdComments = Math.floor(Math.random() * idComments.length);
-  let randomAvatar = Math.floor(Math.random() * avatar.length);
-  let randomMessage =  Math.floor(Math.random() * message.length);
-  let randomName =  Math.floor(Math.random() * name.length);
+
+  const randomAvatar = Math.floor(Math.random() * AVATAR.length);
+  const randomMessage =  Math.floor(Math.random() * MESSAGE.length);
+  const randomName =  Math.floor(Math.random() * name.length);
   return {
-    id: idComments[randomIdComments],
-    avatar:'photos/' + avatar[randomAvatar] +'.jpg',
-    message: message[randomMessage],
+    id: mixIdComments(),
+    avatar:'photos/' + AVATAR[randomAvatar] +'.jpg',
+    message: MESSAGE[randomMessage],
     name: name[randomName],
   }
 }
 
-const createPost = () => {
-  let randomId =  Math.floor(Math.random() * id.length);
-  let randomUrl =  Math.floor(Math.random() * url.length);
-  let randomDescription =  Math.floor(Math.random() * description.length);
-  let randomLikes =Math.floor(Math.random() * (200 - 15)) + 15;
-  return {
-    id: id[randomId],
-    url: 'photos/' + url[randomUrl] +'.jpg',
-    description:  description[randomDescription],
-    likes: randomLikes,
-    comments: createComments,
-  };
-};
-//  console.log(createPost())
 
-//Не пойму как сделать их уникальными и у меня выходит объект в объекте
+// тут конец комментов
+const createPost = () => {
+  const randomDescription =  Math.floor(Math.random() * DESCRIPTION.length);
+  const randomLikes =Math.floor(Math.random() * (200 - 15)) + 15;
+  return {
+    id: mixId(),
+    url: 'photos/' + mixUrls() +'.jpg',
+    description:  DESCRIPTION[randomDescription],
+    likes: randomLikes,
+    comments: createComments(),
+  };
+}
+createPost
+//console.log(createPost())
